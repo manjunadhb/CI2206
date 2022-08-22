13f749cb124d57278d692362b72fb019192daa09
manjunadhb <manjunadhb@gmail.com>
2022-08-22 15:36:21 +0530

    added third heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>This is Continuous Integration, really interesting.</h1>
       <h1>We have corrected the issue, it should work now.</h1>
+      <h1>This is manjunadh checking Continuous Integration.</h1>
     </div>
   );
 }
